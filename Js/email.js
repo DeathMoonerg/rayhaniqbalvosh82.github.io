585f1b39c7dace5fd0e5b289630c91faef104d34
DeathMoonerg <rayhaniqbal82@gmail.com>
2024-09-23 00:46:07 +0800

    Update email.js

--- Js/email.js
+++ Js/email.js
@@ -1,24 +1,24 @@
 const form = document.getElementById('contact-form');
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const nama = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const pesan = document.getElementById('message').value;
-        const apikey = 'https://formspree.io/f/xgvwjlnl'; // API key
-        const url = `https://formspree.io/api/v1/forms/${apikey}/submissions`;
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const nama = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const pesan = document.getElementById('message').value;
 
-        fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name: nama,
-                email: email,
-                message: pesan
-            })
+    const url = 'https://formspree.io/f/xdknrlpn';
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: nama,
+            email: email,
+            message: pesan
         })
+    })
         .then((response) => response.json())
         .then((data) => console.log(data))
         .catch((error) => console.error(error));
-    });
+});
